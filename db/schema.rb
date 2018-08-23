6600a383ec65f9860d4002c04d05c64dc32082ed
Lukasz Czerniak <lfczerniak@gmail.com>
2018-08-23 16:22:29 +0200

    fix: resolved conflicts in schema.db - #4

--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
+
 ActiveRecord::Schema.define(version: 2018_08_23_133256) do
-=======
-ActiveRecord::Schema.define(version: 2018_08_23_131023) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-<<<<<<< HEAD
   create_table "locations", force: :cascade do |t|
     t.string "name"
     t.string "gps"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-=======
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -36,7 +32,7 @@ ActiveRecord::Schema.define(version: 2018_08_23_131023) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
->>>>>>> master
+
   end
 
 end
