6854fa81b892706039bfba72330f79fd809373f5
Lukasz Czerniak <lfczerniak@gmail.com>
2018-08-24 10:51:33 +0200

    Locations added -#4

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'locations/index'
+  get 'locations/new'
+  get 'locations/create'
+  get 'locations/edit'
   root 'welcome#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
