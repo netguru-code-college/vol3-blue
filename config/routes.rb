a47cb1716782cc61909aad41aef213a85e06a211
Krzysztof Kuchna <kkuchna@gmail.com>
2018-08-28 11:16:55 +0200

    Remowe welcome and change menu

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :activities
-  resources :locations, only: [:index, :show, :edit, :update, :destroy, :new, :create]
-  root 'welcome#index'
+  resources :locations
+  root 'activities#index'
   devise_for :users
 end
