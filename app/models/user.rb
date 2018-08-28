e6af732dbe74038b7acfc56dccf45827100aba9e
NikitaGimon <nikitagimon@gmail.com>
2018-08-28 08:52:44 +0200

    Add user profile and redirect after log in

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable
+
+  validates :user_name, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :locations, only: [:index, :show, :edit, :update, :destroy, :new, :create]
   root 'welcome#index'
+  get 'set_location', to: 'users#set_location'
   devise_for :users
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
