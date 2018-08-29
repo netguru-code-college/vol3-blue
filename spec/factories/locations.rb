762119ca550076f7754e2db1f9868e3931a2cf06
Krzysztof Kuchna <kkuchna@gmail.com>
2018-08-29 09:39:56 +0200

    Remove user from activity

--- spec/factories/locations.rb
+++ spec/factories/locations.rb
@@ -10,7 +10,6 @@
 
 FactoryBot.define do
   factory :location do
-    name { Faker::Address.city }
     gps_latitude { Faker::Address.latitude }
     gps_longitude { Faker::Address.longitude }
   end
--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -11,13 +11,5 @@
 require 'rails_helper'
 
 describe Location, type: :model do
-  it 'is valid with name and gps coordinates' do
-    should validate_presence_of(:name)
-    should validate_presence_of(:gps_longitude)
-    should validate_presence_of(:gps_latitude)
-  end
 
-  it 'should have unique name' do
-    should have_db_index(:name).unique(true)
-  end
 end
