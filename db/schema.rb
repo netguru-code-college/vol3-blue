4455815eb7dd5fb27ed8dc34e00babef8f6977da
Krzysztof Kuchna <kkuchna@gmail.com>
2018-08-29 16:55:27 +0200

    Add user to activities

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_29_142537) do
+ActiveRecord::Schema.define(version: 2018_08_29_145348) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2018_08_29_142537) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_activities_on_user_id"
   end
 
   create_table "cities", force: :cascade do |t|
@@ -67,6 +69,7 @@ ActiveRecord::Schema.define(version: 2018_08_29_142537) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "activities", "users"
   add_foreign_key "locations", "activities"
   add_foreign_key "requirements", "activities"
 end
